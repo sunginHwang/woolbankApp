6ad3d2d724de9d00ea6539a252806c43a63950ca
sunginHwang <gommpo111@gmail.com>
2024-11-17 19:04:38 +0900

    feat: test for api cookie

--- src/screens/detail-screen/DetailScreen.tsx
+++ src/screens/detail-screen/DetailScreen.tsx
@@ -1,6 +1,7 @@
 import { BaseText } from '@components/base-text/BaseText';
-import { FC } from 'react';
-import { Text, View } from 'react-native';
+import axios from 'axios';
+import { FC, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 type TabParamList = {
   '가계부 통계': undefined;
@@ -10,9 +11,37 @@ type TabParamList = {
 type Props = any;
 
 export const DetailsScreen: FC<Props> = ({ navigation }) => {
+  const [logText, setLogText] = useState('none');
+
+const loginClick = async () => {
+  const data = await axios.post('https://bank-api.woolta.com/user/share-code-login', {
+    shareCode: '',
+  })
+  setLogText(JSON.stringify(data));
+}
+
+const dataCheck = async () => {
+  setLogText('데이터확인');
+  try {
+    const res = await axios.get(`https://bank-api.woolta.com/account-books?dateTime=${new Date()}`);
+    setLogText('>???');
+    setLogText(JSON.stringify(res));
+    console.log(res);
+  } catch(e) {
+    setLogText('에러남');
+    setLogText(JSON.stringify(e));
+  }
+}
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <BaseText kind='title1Bold' color='gray600' mb={20}>할로우</BaseText>
+         <BaseText onPress={loginClick} kind='title1Bold' color='gray600' mb={20}>로그인체크</BaseText>
+         <BaseText onPress={dataCheck} kind='title1Bold' color='gray600' mb={20}>데이터확인</BaseText>
+       <View style={S.container}>
+        <BaseText  kind='title1Bold' color='gray600' mb={20}>{logText}</BaseText>
+       </View>
+
+      <BaseText onPress={loginClick} kind='title1Bold' color='gray600' mb={20}>할로우</BaseText>
       <Text
         onPress={() => {
           navigation.push('WebView', {
@@ -35,3 +64,12 @@ export const DetailsScreen: FC<Props> = ({ navigation }) => {
     </View>
   );
 };
+
+
+const S = StyleSheet.create({
+  container: {
+    width: '100%',
+    height:300,
+    overflow: 'scroll',
+  },
+});
